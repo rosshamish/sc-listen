b470e748df509fb3c6272aa7fab9e1036cca3f59
Ross Anderson <ross.anderson@ualberta.ca>
2015-10-29 23:56:49 -0600

    Add user search, track fetching, playlist fetching

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 ```
-sc-listen master ❯ sc-listen
-Not yet implemented
 sc-listen master ❯ sc-listen help
 NAME:
    sc-listen - stream soundcloud from the terminal
@@ -18,3 +16,43 @@ GLOBAL OPTIONS:
    --help, -h		show help
    --version, -v	print the version
 ```
+
+Build
+
+`go build`
+
+Run
+
+`./sc-listen [username]`
+
+```
+sc-listen master* ❯ ./sc-listen troyboi
+Did you mean...
+	%s TroyBoi
+	%s TroyBoi Onlygodcanjudgeme
+	%s troyboi924
+
+Found user TroyBoi
+	Full Name: 
+	Country: 
+Tracks:
+	TroyBoi: TroyBoi Feat NEFERA - On My Own
+	TroyBoi: Tinie Tempah feat Jess Glynne - Not Letting Go (TroyBoi Remix)
+	TroyBoi: Wonky
+	TroyBoi: TroyBoi - BBC Radio 1 Guest Mix - DJ Target
+	TroyBoi: ili
+	...
+Playlists:
+	TroyBoi: TrapMusic.NET Uploads
+	TroyBoi: SoundSnobz
+	TroyBoi: icekream
+```
+
+---
+
+useful resources:
+- github.com/moriyoshi/pulsego/ to play to speakers
+- golang-soundcloud as a soundcloud api wrapper (and probably contribute back to this)
+- github.com/mdlayher/waveform for printing a waveform soundcloud-style
+- https://developers.soundcloud.com/docs/api/reference
+
--- main.go
+++ main.go
@@ -1,18 +1,85 @@
 package main
 
 import (
-  "os"
-  "github.com/codegangsta/cli"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"github.com/yanatan16/golang-soundcloud/soundcloud"
+	"net/url"
+	"os"
 )
 
-func main() {
-  app := cli.NewApp()
-  app.Name = "sc-listen"
-  app.Usage = "stream soundcloud from the terminal"
-  app.Action = func(c *cli.Context) {
-    println("Not yet implemented")
-  }
+func appAction(c *cli.Context) {
+	var scUser string
+	scUser = "rosshamish"
+	if len(c.Args()) > 0 {
+		scUser = c.Args()[0]
+	}
 
-  app.Run(os.Args)
+	scapi := &soundcloud.Api{
+		ClientId: "73275ddb505720361c57d1f3b3e837c0",
+	}
+
+	users, err := scapi.Users(url.Values{"q": []string{scUser}})
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else if len(users) == 0 {
+		fmt.Println("No users found. Exiting.")
+		return
+	} else if len(users) > 1 {
+		fmt.Println("Did you mean...")
+		for i, user := range users {
+			if i >= 3 {
+				break
+			}
+			fmt.Println("\t%s", user.Username)
+		}
+		fmt.Println()
+	}
+
+	user := users[0]
+	fmt.Printf("Found user %s\n", user.Username)
+	fmt.Printf("\tFull Name: %s\n", user.FullName)
+	fmt.Printf("\tCountry: %s\n", user.Country)
+
+	uapi := scapi.User(user.Id)
+	tracks, err := uapi.Tracks(url.Values{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Tracks:")
+	for i, track := range tracks {
+		if i >= 5 {
+			fmt.Printf("\t...\n")
+			break
+		}
+		fmt.Printf("\t%s: %s\n", user.Username, track.Title)
+		//fmt.Println("\t\t%s\n", track.StreamUrl)
+	}
+
+	playlists, err := uapi.Playlists(url.Values{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Playlists:")
+	for i, playlist := range playlists {
+		if i >= 5 {
+			fmt.Println("\t...")
+			break
+		}
+		fmt.Printf("\t%s: %s\n", user.Username, playlist.Title)
+	}
 }
 
+func main() {
+	app := cli.NewApp()
+	app.Name = "sc-listen"
+	app.Usage = "stream soundcloud from the terminal"
+	app.Action = appAction
+
+	app.Run(os.Args)
+}
